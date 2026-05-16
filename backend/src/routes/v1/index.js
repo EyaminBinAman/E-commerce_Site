5b32db5dff22ebda0f5e7e5bd03db41848b82b28
Sadim Fahad <Sadimfahad9@gmail.com>
2026-05-16 21:34:28 +0600

    Added animals API as per instruction and tested with postman

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./userRoutes.js");
+const animalRoutes = require("./animalRoutes.js");
 
 
 router.use("/users", userRoutes);
+router.use("/animals", animalRoutes);
 
 module.exports = router;
